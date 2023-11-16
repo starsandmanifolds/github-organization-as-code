90b309ad1d67f77d46ac3ae2706135ccff80a39d
Anand Dyavanapalli <adyavanapalli@veradigm.com>
2023-11-16 02:11:34 -0500

    chore: Setup Terraform Cloud state file storage.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+    organization = "starsandmanifolds"
+
+    workspaces {
+      name = "github-organization-as-code"
+    }
+  }
+
   required_providers {
     github = {
       source = "integrations/github"
