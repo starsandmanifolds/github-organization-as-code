4e1fceb70eb84662616926cecba84680fad88c1a
Anand Dyavanapalli <adyavanapalli@veradigm.com>
2023-11-16 02:24:28 -0500

    chore: Add some teams.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,14 @@
 resource "github_repository" "repository" {
-  name        = "sample-repo"
-  description = "This is a sample repo."
+  name        = "az-tf-app"
+  description = "Contains the Terraform configuration for the application on Azure."
+}
+
+resource "github_team" "teams" {
+  for_each = {
+    "az-tf-pr-approvers" = "Responsible for approving pull requests in repositories that hold Terraform configuration for Azure resources."
+    "gitops"             = "Responsible for all gitops happening in this organization."
+  }
+
+  name        = each.key
+  description = each.value
 }
