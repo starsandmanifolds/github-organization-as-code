a04e62cb040e7476b9003f58a9c49aa1a47d734c
Anand Dyavanapalli <adyavanapalli@veradigm.com>
2023-11-16 02:54:54 -0500

    chore: try this.

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,43 @@ resource "github_team" "teams" {
   description = each.value
   privacy     = "closed"
 }
+
+locals {
+  repository_environments = {
+    "az-tf-app" = {
+      environments = ["Development", "Staging", "Production"]
+      teams        = ["gitops"]
+    }
+  }
+}
+
+resource "github_repository_environment" "repository_environments" {
+  for_each = merge(
+    [
+      for repository, info in local.repository_environments :
+      {
+        for environment in info.environments :
+        "${repository} - ${environment}" => {
+          repository = repository
+          environment = environment
+          teams = info.teams
+        }
+      }
+    ]...
+  )
+
+  environment  = each.value.environment
+  repository   = github_repository.github_repositories[each.value.repository].name
+
+  reviewers {
+    teams = [
+      for team in each.value.teams :
+      github_team.teams[team].id
+    ]
+  }
+
+  deployment_branch_policy {
+    protected_branches     = true
+    custom_branch_policies = false
+  }
+}
